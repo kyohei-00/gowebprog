1fcf662d9b78d6c95b59881d31715c9c29d2f51c
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-10 21:54:55 +0900

    テンプレートエンジンを実装

--- cmd/ch02/chitchat/route_main.go
+++ cmd/ch02/chitchat/route_main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	threads, error := data.Threads()
+	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(w, r)
-		public_tmpl_files := []string{
-			"templates/layout.html", "templates/public.navbar.html", "templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html", "templates/private.navbar.html", "templates/index.html",
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(w, threads, "layout", "private.navbar", "index")
 		}
 	}
-
 }
